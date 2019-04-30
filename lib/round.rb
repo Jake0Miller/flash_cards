18b80af43bf22ff81ff04d508e63063168616043
Jake Miller <jake0miller@gmail.com>
2019-04-30 13:51:56 -0600

    Add empty class method framework

--- lib/round.rb
+++ lib/round.rb
@@ -1,6 +1,30 @@
 class Round
+  attr_reader :deck, :turns, :current_card, :number_correct
 
   def initialize(deck)
     @deck = deck
+    @turns = []
+    if deck.length > 0
+      @current_card = deck[0]
+    else
+      @current_card = nil
+    end
+    @number_correct = 0
+  end
+
+  def take_turn(guess)
+
+  end
+
+  def number_correct_by_category(category)
+
+  end
+
+  def percent_correct
+
+  end
+
+  def percent_correct_by_category
+
   end
 end
