9c45e2ae95bfdb82e7ba2f2d588003a09a1112a6
Jake Miller <jake0miller@gmail.com>
2019-05-01 13:56:33 -0600

    Update round.rb, deck_test.rb, round_test.rb to use new deck.count_cards method name

---
+++ lib/round.rb
@@ -0,0 +1,59 @@
+class Round
+  attr_reader :deck, :turns, :current_card,
+    :number_correct, :discard_pile
+
+  def initialize(deck)
+    @deck = deck
+    @turns = []
+    @discard_pile = []
+    @number_correct = 0
+    set_first_card
+  end
+
+  def set_first_card
+    if @deck.count_cards > 0
+      @current_card = @deck.cards.shift
+    else
+      p "We're out of cards!"
+      @current_card = nil
+    end
+  end
+
+  def take_turn(guess)
+    this_turn = Turn.new(guess,@current_card)
+    if this_turn.correct?
+      @number_correct += 1
+    end
+    @discard_pile.push(@current_card)
+    if @deck.count_cards > 0
+      @current_card = deck.cards.shift
+    else
+      p "We're out of cards!"
+      @current_card = nil
+    end
+    @turns.push(this_turn)
+    this_turn
+  end
+
+  def number_correct_by_category(category)
+    category_list = @turns.select do |turn|
+      turn.card.category == category && turn.correct?
+    end
+    category_list.length
+  end
+
+  def percent_correct
+    (@number_correct / @turns.length.to_f).round(2)
+  end
+
+  def number_turns_by_category(category)
+    category_list = @turns.select do |turn|
+      turn.card.category == category
+    end
+    category_list.length
+  end
+
+  def percent_correct_by_category(category)
+    (number_correct_by_category(category) / number_turns_by_category(category).to_f).round(2)
+  end
+end
