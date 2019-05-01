ed242656034c3ba95f04f7272b36ef1030a47466
Jake Miller <jake0miller@gmail.com>
2019-05-01 16:49:33 -0600

    Add setup method to turn_test.rb

--- test/turn_test.rb
+++ test/turn_test.rb
@@ -4,66 +4,47 @@ require './lib/card'
 require './lib/turn'
 
 class TurnTest < Minitest::Test
+  def setup
+    @card = Card.new("What is the capital of Alaska?", "Juneau", :Geograph)
+    @turn = Turn.new("Juneau", @card)
+  end
 
   def test_it_exists
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geograph)
-    turn = Turn.new("Juneau", card)
-
-    assert_instance_of Turn, turn
+    assert_instance_of Turn, @turn
   end
 
   def test_it_has_a_guess
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
-
-    assert_instance_of String, turn.guess
+    assert_instance_of String, @turn.guess
   end
 
   def test_it_has_a_card
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
-
-    assert_instance_of Card, turn.card
+    assert_instance_of Card, @turn.card
   end
 
   def test_guess_is_correct
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
-
-    assert_equal "Juneau", turn.guess
+    assert_equal "Juneau", @turn.guess
   end
 
   def test_guess_is_incorrect
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Seattle", card)
+    turn = Turn.new("Seattle", @card)
 
     refute "Juneau" == turn.guess
   end
 
   def test_correct_guess_is_correct
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
-
-    assert_equal true, turn.correct?
+    assert_equal true, @turn.correct?
   end
 
   def test_incorrect_guess_is_incorrect
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Seattle", card)
-
-    assert_equal false, turn.correct?
+    assert_equal false, @turn.correct?
   end
 
   def test_correct_guess_feedback
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Juneau", card)
-
-    assert_equal "Correct!", turn.feedback
+    assert_equal "Correct!", @turn.feedback
   end
 
   def test_incorrect_guess_is_incorrect
-    card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    turn = Turn.new("Seattle", card)
+    turn = Turn.new("Seattle", @card)
 
     assert_equal "Incorrect.", turn.feedback
   end
