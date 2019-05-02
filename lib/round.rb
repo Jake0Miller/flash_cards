0611bb347c7a318e5bc981a12ca6e59e86618f9f
Jake Miller <jake0miller@gmail.com>
2019-05-02 10:22:50 -0600

    Optimize code by replacing 4 loops with 1 hash

--- lib/round.rb
+++ lib/round.rb
@@ -9,6 +9,7 @@ class Round
     @turns = []
     @discard_pile = []
     @number_correct = 0
+    @num_correct_and_num_cards_by_category = Hash.new
     set_first_card
   end
 
@@ -23,15 +24,32 @@ class Round
 
   def take_turn(guess)
     this_turn = Turn.new(guess,@current_card)
+
+    cur_cat = @current_card.category
     if this_turn.correct?
+      if @num_correct_and_num_cards_by_category[cur_cat] == nil
+        @num_correct_and_num_cards_by_category[cur_cat] = [1,1]
+      else
+        @num_correct_and_num_cards_by_category[cur_cat][0] += 1
+        @num_correct_and_num_cards_by_category[cur_cat][1] += 1
+      end
       @number_correct += 1
+    else
+      if @num_correct_and_num_cards_by_category[cur_cat] == nil
+        @num_correct_and_num_cards_by_category[cur_cat] = [0,1]
+      else
+        @num_correct_and_num_cards_by_category[cur_cat][1] += 1
+      end
     end
+
     @discard_pile.push(@current_card)
+
     if @deck.count_cards > 0
       @current_card = deck.cards.shift
     else
       shuffle_cards
     end
+
     @turns.push(this_turn)
     this_turn
   end
@@ -44,10 +62,9 @@ class Round
   end
 
   def number_correct_by_category(category)
-    category_list = @turns.select do |turn|
-      turn.card.category == category && turn.correct?
-    end
-    category_list.length
+    #category_list = @turns.select { |turn| turn.card.category == category && turn.correct? }
+    #category_list.length
+    @num_correct_and_num_cards_by_category[category][0]
   end
 
   def percent_correct
@@ -55,10 +72,11 @@ class Round
   end
 
   def number_turns_by_category(category)
-    category_list = @turns.select do |turn|
-      turn.card.category == category
-    end
-    category_list.length
+    #category_list = @turns.select do |turn|
+      #turn.card.category == category
+    #end
+    #category_list.length
+    @num_correct_and_num_cards_by_category[category][1]
   end
 
   def percent_correct_by_category(category)
@@ -66,13 +84,14 @@ class Round
   end
 
   def get_all_categories
-    categories = Set[]
-    @deck.cards.each do |card|
-      categories.add(card.category)
-    end
-    @discard_pile.each do |card|
-      categories.add(card.category)
-    end
-    categories
+    #categories = Set[]
+    #@deck.cards.each do |card|
+      #categories.add(card.category)
+    #end
+    #@discard_pile.each do |card|
+      #categories.add(card.category)
+    #end
+    #categories
+    @num_correct_and_num_cards_by_category.keys
   end
 end
