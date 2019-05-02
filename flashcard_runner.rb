197766f01fcb5f43cb9961b5a60230150facd8c4
Jake Miller <jake0miller@gmail.com>
2019-05-02 15:18:37 -0600

    Change flashcard_runner.rb to use card_generator

--- flashcard_runner.rb
+++ flashcard_runner.rb
@@ -6,12 +6,6 @@ require './lib/card_generator'
 require 'pry'
 
 cards = CardGenerator.new("cards.txt").cards
-
-# alaska_card = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-# mars_card = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-# direction_card = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-# cards = [alaska_card, mars_card, direction_card]
-
 deck = Deck.new(cards)
 round = Round.new(deck)
 
