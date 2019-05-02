290fec435ca00cceefb6bc73b0fabc1a06e3a85c
Jake Miller <jake0miller@gmail.com>
2019-05-02 15:38:27 -0600

    Fix card_generator to support multi-word symbols

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -12,6 +12,7 @@ class CardGenerator
     cards = []
     IO.foreach(@file) do |line|
       split_line = line.chomp.split(',')
+      split_line[2].gsub!(/ /, '_')
       cards << Card.new(split_line[0],split_line[1],split_line[2].to_sym)
     end
     cards
